6d9b6d4f3c63bae29322022f08fc448f8ba7de38
fontanit <tristan.fontaniere@etu.univ-grenoble-alpes.fr>
2025-02-24 20:52:15 +0100

    Writing of the different tests for the application using assert library

--- src/test.c
+++ src/test.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
 #include "calc.h"
+#include <stdlib.h>
+#include <assert.h>
 
 void run_tests()
 {
+    //Tests of the add function
+    assert(add(1,2) == 3);
+    assert(add(1,-1) == 0);
+    
+    //Tests of the subtract function
+    assert(subtract(3,2) == 1);
+    assert(subtract(2,3) == -1);
+
+    //Tests of the multiply function
+    assert(multiply(3,2) == 6);
+    assert(multiply(3,-2) == -6);
+
+    //Tests of the divide function
+    assert(divide(10,2) == 5);
+    assert(divide(10,-2) == -5);
+    assert(divide(10,0) == 0);
+
 }
 
 int main()
 {
     run_tests();
-    return 0;
+    return EXIT_SUCCESS;
 }
